7675df678790aef02879d6ee874cb3793e94b4d1
apayne28 <payneashley1208@gmail.com>
2022-08-01 20:42:09 -0400

    Fixed index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, HashRouter } from "react-router-dom";
-import { createRoot } from "react-dom/client";
+
 import "./assets/scss/main.css";
 import App from "./App";
 
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
-
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <HashRouter>
       <App />
